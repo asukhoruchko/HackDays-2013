b2448be34695cb451ef7e80658e8b7b806cee2ec
Alexey Soukhoruchko <alexey.soukhoruchko@viacode.com>
2013-11-02 13:53:35 +0400

    DB

---
+++ Umbrella-DB/OperationLog.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[OperationLog]
+(
+    [Operation_Log_Id] INT NOT NULL PRIMARY KEY,
+    [User_Id] INT NOT NULL REFERENCES [dbo].[User](User_ID),
+    [Date_Taken] Date NOT NULL,
+    [Date_Returned] Date NOT NULL
+)
---
+++ Umbrella-DB/User.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [User_Id] INT NOT NULL PRIMARY KEY,
+    [Login] nvarchar(255) NOT NULL
+)
